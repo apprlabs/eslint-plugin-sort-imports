9699f81cfda5dc6bc4fbe98372d924a085736c02
Troy McCabe <troymccabe@gmail.com>
2024-12-16 22:28:33 -0600

    add step to convert

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 export default {
+    collectCoverage: true,
+    coverageReporters: [
+        ['text', { file: 'coverage.txt', path: './coverage/.' }]
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+    },
     preset: 'ts-jest',
     testEnvironment: 'node'
 };
